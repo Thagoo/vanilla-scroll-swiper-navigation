0ad26a45ed12e552a90f83164771364bb05633ef
thagoo <lohitgowda56@gmail.com>
2024-04-05 11:06:44 +0530

    Add Text headlines on path slider for all sections

--- scrips/index.js
+++ scrips/index.js
@@ -1,28 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.to(".dotsfill1", { fill: "#000", opacity: 0 });
-
-  const sections = document.querySelectorAll(".content-section");
-  sections.forEach((section, index) => {});
-});
-
-document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
-  function animateImages(section) {
-    const imagesContainer = section.querySelector(".images-container");
-    gsap.to(imagesContainer, {
-      yPercent: -100 * (imagesContainer.children.length - 1),
-      ease: "none",
-      scrollTrigger: {
-        trigger: section,
-        start: "top top",
-        end: () =>
-          `+=${section.offsetHeight * (imagesContainer.children.length - 1)}`,
-        scrub: true,
-        pin: true,
-        anticipatePin: 1,
-      },
-    });
-  }
 
   const sections = gsap.utils.toArray(".content-section");
   let currentSectionIndex = 0;
@@ -36,6 +13,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         onEnter: () => {
           // Show this section
           section.style.display = "flex";
+
           // Animate the images in column 2
           // animateImages(section);
           // Hide previous section
@@ -58,6 +36,40 @@ document.addEventListener("DOMContentLoaded", (event) => {
     }
   });
 
+  // Path slider updates
+  const bgColors = [
+    "bg-purple-900",
+    "bg-indigo-700",
+    "bg-indigo-950",
+    "bg-sky-950",
+    "bg-sky-700",
+    "bg-blue-900",
+    "bg-emerald-700",
+  ];
+  const pathSlider = document.getElementById("path-slider");
+  const currentBgColor = /\bbg-.*?0\b/g;
+
+  function upgradePathSlider(index) {
+    pathSlider.className = pathSlider.className.replace(
+      currentBgColor,
+      bgColors[index]
+    );
+    gsap.to(`.dotsfill${index + 1}`, {
+      fill: "rgb(0, 146, 255)",
+      opacity: 1,
+    });
+  }
+  function downgradePathSlider(index) {
+    pathSlider.className = pathSlider.className.replace(
+      currentBgColor,
+      bgColors[index]
+    );
+    gsap.to(`.dotsfill${index + 2}`, {
+      fill: "rgb(0, 146, 255)",
+      opacity: 0,
+    });
+  }
+
   // Function to display the current section and hide others
   function goToSection(index) {
     sections.forEach((section, i) => {
@@ -71,15 +83,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
   // Add listeners for mouse wheel events
   window.addEventListener("wheel", (e) => {
     e.preventDefault();
+
     const direction = e.deltaY > 0 ? 1 : -1;
     if (direction === 1 && currentSectionIndex < sections.length - 1) {
       // Scrolling down
       currentSectionIndex++;
       goToSection(currentSectionIndex);
+      upgradePathSlider(currentSectionIndex);
     } else if (direction === -1 && currentSectionIndex > 0) {
       // Scrolling up
       currentSectionIndex--;
       goToSection(currentSectionIndex);
+      downgradePathSlider(currentSectionIndex);
     }
   });
 });
